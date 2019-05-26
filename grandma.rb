ab060f6f6fa5b154e5a7430ebcd19d7409141846
Andrew Wester <awester17@gmail.com>
2019-05-26 14:04:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,7 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-def speak_to_grandma(string)
-  
-end
+
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
@@ -13,3 +11,8 @@ end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+end
